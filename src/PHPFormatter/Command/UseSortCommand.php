71a95f421a9778741fbb0aee0bacb48532a3e55d
Marc <yuhu@mmoreram.com>
2014-08-03 19:27:37 +0200

    Optimized command methods

--- src/PHPFormatter/Command/UseSortCommand.php
+++ src/PHPFormatter/Command/UseSortCommand.php
@@ -16,6 +16,7 @@
 namespace Mmoreram\PHPFormatter\Command;
 
 use Exception;
+use IteratorAggregate;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -104,37 +105,12 @@ class UseSortCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $verbose = $output->getVerbosity();
         $path = $input->getArgument('path');
-        $dryRun = $input->getOption('dry-run');
-        $fileFinder = new FileFinder;
-        $configLoader = new ConfigLoader;
-        $configFinder = new ConfigFinder;
 
         /**
-         * This section is just for finding the right values to work with in
-         * this execution.
-         *
-         * $options array will have, after this block, all these values
+         * We load the options to work with
          */
-        $configPath = rtrim($input->getOption('config'), DIRECTORY_SEPARATOR);
-
-        $options = $configLoader->loadConfigValues(
-            self::COMMAND_NAME,
-            $configFinder->findConfigFile($configPath),
-            array(
-                'group' => $input->getOption('group'),
-                'group-type' => $input->getOption('group-type'),
-                'sort-type' => $input->getOption('sort-type'),
-                'sort-direction' => $input->getOption('sort-direction')
-            ),
-            array(
-                'group' => array('_main'),
-                'group-type' => UseSorter::GROUP_TYPE_EACH,
-                'sort-type' => UseSorter::SORT_TYPE_ALPHABETIC,
-                'sort-direction' => UseSorter::SORT_DIRECTION_ASC
-            )
-        );
+        $options = $this->getUsableConfig($input);
 
         /**
          * Building the real directory or file to work in
@@ -150,6 +126,93 @@ class UseSortCommand extends Command
         }
 
         /**
+         * Print dry-run message if needed
+         */
+        $this->printDryRunMessage(
+            $input,
+            $output,
+            $path
+        );
+
+        /**
+         * Print all configuration block if verbose level allows it
+         */
+        $this->printConfigUsed(
+            $output,
+            $options
+        );
+
+        $fileFinder = new FileFinder;
+        $files = $fileFinder->findPHPFilesByPath($path);
+
+        /**
+         * Parse and fix all found files
+         */
+        $this->parseAndFixFiles(
+            $input,
+            $output,
+            $files,
+            $options
+        );
+    }
+
+    /**
+     * Load config
+     *
+     * @param InputInterface $input Input
+     *
+     * @return array Config array
+     */
+    public function getUsableConfig(InputInterface $input)
+    {
+        $configLoader = new ConfigLoader;
+        $configFinder = new ConfigFinder;
+
+        /**
+         * This section is just for finding the right values to work with in
+         * this execution.
+         *
+         * $options array will have, after this block, all these values
+         */
+        $configPath = rtrim($input->getOption('config'), DIRECTORY_SEPARATOR);
+
+        return $configLoader->loadConfigValues(
+            self::COMMAND_NAME,
+            $configFinder->findConfigFile($configPath),
+            array(
+                'group' => $input->getOption('group'),
+                'group-type' => $input->getOption('group-type'),
+                'sort-type' => $input->getOption('sort-type'),
+                'sort-direction' => $input->getOption('sort-direction')
+            ),
+            array(
+                'group' => array('_main'),
+                'group-type' => UseSorter::GROUP_TYPE_EACH,
+                'sort-type' => UseSorter::SORT_TYPE_ALPHABETIC,
+                'sort-direction' => UseSorter::SORT_DIRECTION_ASC
+            )
+        );
+    }
+
+    /**
+     * Print the Dry-run message if needed
+     *
+     * @param InputInterface  $input  Input
+     * @param OutputInterface $output Output
+     * @param string          $path   Path
+     *
+     * @return UseSortCommand self Object
+     */
+    public function printDryRunMessage(
+        InputInterface $input,
+        OutputInterface $output,
+        $path
+    )
+    {
+        $dryRun = $input->getOption('dry-run');
+        $verbose = $output->getVerbosity();
+
+        /**
          * Dry-run message
          */
         if ($dryRun && $verbose >= OutputInterface::VERBOSITY_VERBOSE) {
@@ -162,17 +225,23 @@ class UseSortCommand extends Command
             $output->writeln('# Executing process in ' . $path);
         }
 
-        /**
-         * Creates the new UseSorter file, given config values
-         */
-        $useSorter = new UseSorter();
-        $useSorter
-            ->setGroups($options['group'])
-            ->setGroupType($options['group-type'])
-            ->setSortType($options['sort-type'])
-            ->setSortDirection($options['sort-direction']);
+        return $this;
+    }
 
-        $files = $fileFinder->findPHPFilesByPath($path);
+    /**
+     * Print the configuration used by the command
+     *
+     * @param OutputInterface $output  Output
+     * @param array           $options Options used by the command
+     *
+     * @return UseSortCommand self Object
+     */
+    public function printConfigUsed(
+        OutputInterface $output,
+        array $options
+    )
+    {
+        $verbose = $output->getVerbosity();
 
         /**
          * If verbose level is higher or equal than -vv, we print the config
@@ -207,6 +276,31 @@ class UseSortCommand extends Command
 
         $output->writeln('#');
 
+        return $this;
+    }
+
+    /**
+     * Parse and fix all files found
+     *
+     * @param InputInterface    $input   Input
+     * @param OutputInterface   $output  Output
+     * @param IteratorAggregate $files   Files
+     * @param array             $options Options
+     *
+     * @return UseSortCommand self Object
+     */
+    public function parseAndFixFiles(
+        InputInterface $input,
+        OutputInterface $output,
+        IteratorAggregate $files,
+        array $options
+
+    )
+    {
+        $dryRun = $input->getOption('dry-run');
+        $verbose = $output->getVerbosity();
+        $useSorter = $this->createUseSorter($options);
+
         /**
          * Each found php file is processed
          */
@@ -230,5 +324,29 @@ class UseSortCommand extends Command
                 file_put_contents($file, $result);
             }
         }
+
+        return $this;
+    }
+
+    /**
+     * Create UseSorter Object given a configuration
+     *
+     * @param array $options Options
+     *
+     * @return UseSorter Use sorter instance
+     */
+    public function createUseSorter(array $options)
+    {
+        /**
+         * Creates the new UseSorter file, given config values
+         */
+        $useSorter = new UseSorter();
+        $useSorter
+            ->setGroups($options['group'])
+            ->setGroupType($options['group-type'])
+            ->setSortType($options['sort-type'])
+            ->setSortDirection($options['sort-direction']);
+
+        return $useSorter;
     }
 }
