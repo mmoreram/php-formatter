daa6c73e57eb92ccc71c2aa963a1feec9b840449
Michael Jett <jumbojett@users.noreply.github.com>
2015-09-17 15:58:55 -0400

    Keeping original CR

--- src/PHPFormatter/Finder/FileFinder.php
+++ src/PHPFormatter/Finder/FileFinder.php
@@ -1,15 +1,16 @@
 <?php
 
 /*
- * This file is part of MITRE's ACE project
+ * This file is part of the php-formatter package
  *
- * Copyright (c) 2015 MITRE Corporation
+ * Copyright (c) 2014 Marc Morera
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
+ * Feel free to edit as you please, and have fun.
  *
- * @author MITRE's ACE Team <ace-team@mitre.org>
+ * @author Marc Morera <yuhu@mmoreram.com>
  */
 
 namespace Mmoreram\PHPFormatter\Finder;
