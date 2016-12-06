b3339fe4dc54af01af773f4e60a825ac7d7386df
Marc Morera <yuhu@mmoreram.com>
2016-12-06 13:55:08 +0100

    Fixed autoload

---
+++ src/bootstrap.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file is part of the php-formatter package
+ *
+ * Copyright (c) >=2014 Marc Morera
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ */
+
+declare(strict_types=1);
+
+function includeIfExists($file)
+{
+    return file_exists($file) ? include $file : false;
+}
+
+if ((!$loader = includeIfExists(__DIR__ . '/../vendor/autoload.php')) && (!$loader = includeIfExists(__DIR__ . '/../../../autoload.php'))) {
+    echo 'You must set up the project dependencies, run the following commands:' . PHP_EOL .
+        'curl -sS https://getcomposer.org/installer | php' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL;
+    exit(1);
+}
+
+return $loader;
